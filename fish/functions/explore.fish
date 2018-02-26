a6c2b1921a3a504acd70b677d4d9e2cfff9b97ad
Michalis Pardalos <mpardalos@gmail.com>
2018-02-26 19:05:31 +0000

    [fish] explore accepts zero arguments

--- fish/functions/explore.fish
+++ fish/functions/explore.fish
@@ -1,4 +1,9 @@
 function explore
-    dolphin $argv >/dev/null 2>&1 &
+    if count $argv >/dev/null
+        dolphin $argv >/dev/null 2>&1 &
+    else
+        dolphin . >/dev/null 2>&1 &
+    end
+
     disown %dolphin
 end
