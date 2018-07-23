0d397ccefe41266bbd57860e38e1b9040729fb80
Michalis Pardalos <mpardalos@gmail.com>
2018-07-23 14:01:42 +0300

    [lemonbar] Add layout indicator using skb

--- lemonbar/bar.py
+++ lemonbar/bar.py
@@ -1,14 +1,26 @@
 #!/bin/env python3
 from pathlib import Path
 import os
-import shutil
 
-from lemonsqueezer.modules import BSPWMDesktops, Battery, CurrentWifi, Clock, MediaControls
+from lemonsqueezer.modules import BSPWMDesktops, Battery, CurrentWifi, Clock,\
+        CommandMonitorModule, MediaControls
 from lemonsqueezer import Align
 import lemonsqueezer as lsq
 
 pid_file = Path('/tmp/lemonsqueezer_pid')
 pid_file.write_text(str(os.getpid()))
+
+
+class LayoutIndicator(CommandMonitorModule):
+    def __init__(self, *args, **kwargs):
+        super().__init__(['skb'], *args, **kwargs)
+
+    def first_output(self, bar):
+        return
+
+    def on_line_received(self, process_output):
+        return process_output
+
 
 bar = lsq.Bar(
     config_file=str(Path.home() / '.config/lemonbar/colors.json'),
@@ -16,18 +28,25 @@ bar = lsq.Bar(
         "Font Awesome 5 Free:style=Solid", "Noto Sans Display"
     ],
     geometry=(1900, 30, 10, 10),
-        bg_color="#DD6a1931", 
-        fg_color="#FFFFFF", 
-        padding=(20, 20), 
-        spacing=10,
-        separator='│')
+    bg_color="#DD6a1931",
+    fg_color="#FFFFFF",
+    padding=(20, 20),
+    spacing=10,
+    offset=-2,
+    separator='│')
 
-bar.register_module(BSPWMDesktops(monitor_sel='focused', highlight_color='#4788ef'))
-bar.register_module(MediaControls('{artist} - {title}', ('', '','', ''),
-    icon='', align=Align.CENTER, u_color='#AA1156'))
+bar.register_module(
+    BSPWMDesktops(monitor_sel='focused', highlight_color='#4788ef'))
+bar.register_module(
+    MediaControls(
+        '{artist} - {title}', ('', '', '', ''),
+        icon='',
+        align=Align.CENTER))
+bar.register_module(LayoutIndicator(align=Align.RIGHT))
 bar.register_module(Battery(['', '', '', '', ''], align=Align.RIGHT))
 bar.register_module(CurrentWifi('wlp58s0', icon='', align=Align.RIGHT))
-bar.register_module(Clock("+%a %d %b - %H:%M", 20, icon='', align=Align.RIGHT))
+bar.register_module(
+    Clock("+%a %d %b - %H:%M", 20, icon='', align=Align.RIGHT))
 
 try:
     bar.run()
