039ae78915be8a99bc601a331198100b5502139f
Michalis Pardalos <m.pardalos@gmail.com>
2018-08-27 01:05:20 +0300

    [i3lock] Cutsom i3lock with i3lock-color

--- bin/post_wal.sh
+++ bin/post_wal.sh
@@ -5,6 +5,7 @@ cp ~/.cache/wal/rofi_fullscreen.rasi ~/.config/rofi/fullscreen.rasi
 
 cp ~/.cache/wal/polybar ~/.config/polybar/theme
 cp ~/.cache/wal/colors.sh ~/.bin/colors.sh
+cp ~/.cache/wal/colors-rgba.sh ~/.bin/colors-rgba.sh
 
 cp ~/.cache/wal/lemonsqueezer.json ~/.config/dotfiles/lemonbar/colors.json
 kill -10 $(cat /tmp/lemonsqueezer_pid)
