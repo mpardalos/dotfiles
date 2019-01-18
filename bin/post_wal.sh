328dfaecaac2e17860c140469e29b4e97944d685
Michalis Pardalos <m.pardalos@gmail.com>
2019-01-18 15:09:00 +0000

    Fix post_wal.sh

--- bin/post_wal.sh
+++ bin/post_wal.sh
@@ -3,12 +3,11 @@
 cp ~/.cache/wal/rofi_flat.rasi       ~/.config/rofi/flat.rasi
 cp ~/.cache/wal/rofi_fullscreen.rasi ~/.config/rofi/fullscreen.rasi
 
-cp ~/.cache/wal/polybar ~/.config/polybar/theme
 cp ~/.cache/wal/colors.sh ~/.bin/colors.sh
 cp ~/.cache/wal/colors-rgba.sh ~/.bin/colors-rgba.sh
 
 cp ~/.cache/wal/lemonsqueezer.json ~/.config/dotfiles/lemonbar/colors.json
-kill -10 $(cat /tmp/lemonsqueezer_pid)
+kill -10 $(cat /tmp/lemonsqueezer.lock)
 
 ~/.bin/set_bspwm_colors
 
