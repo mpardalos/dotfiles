f433a4ed951511af824638dd0c77e6ad9dc4bcae
Michalis Pardalos <m.pardalos@gmail.com>
2019-04-26 13:06:00 +0300

    Remove unused packages from emacs

--- doom_emacs/packages.el
+++ doom_emacs/packages.el
@@ -1,19 +1,8 @@
 ;; -*- no-byte-compile: t; -*-
-;;; ~/.doom.d/packages.el
-
-;;; Examples:
-;; (package! some-package)
-;; (package! another-package :recipe (:fetcher github :repo "username/repo"))
-;; (package! builtin-package :disable t)
 
 (package! gruvbox-theme)
 (package! base16-theme)
 
 (package! bison-mode)
-(package! eglot)
-;; (package! lsp-mode)
-;; (package! lsp-ui)
-;; (package! company-lsp)
-(package! yasnippet-snippets)
 
 (package! evil-snipe :disable t)
