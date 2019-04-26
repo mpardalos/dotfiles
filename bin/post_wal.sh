2ad280bb26db1aac715f334b46960741f27cc462
Michalis Pardalos <m.pardalos@gmail.com>
2019-04-26 13:09:12 +0300

    Add config for zathura

--- bin/post_wal.sh
+++ bin/post_wal.sh
@@ -8,5 +8,7 @@ cp ~/.cache/wal/colors-rgba.sh ~/.bin/colors-rgba.sh
 cp ~/.cache/wal/lemonsqueezer.json ~/.config/dotfiles/lemonbar/colors.json
 kill -10 $(cat /tmp/lemonsqueezer.lock)
 
+cp ~/.cache/wal/zathuracolors ~/.config/zathura/
+
 ~/.bin/set_bspwm_colors
 
