79edfd6d67eab8a344f45e01232da70fb78b3a56
Michalis Pardalos <m.pardalos@gmail.com>
2019-05-03 00:41:40 +0100

    Switch to polybar

--- bin/post_wal.sh
+++ bin/post_wal.sh
@@ -5,10 +5,9 @@ cp ~/.cache/wal/*.rasi ~/.config/rofi
 cp ~/.cache/wal/colors.sh ~/.bin/colors.sh
 cp ~/.cache/wal/colors-rgba.sh ~/.bin/colors-rgba.sh
 
-cp ~/.cache/wal/lemonsqueezer.json ~/.config/dotfiles/lemonbar/colors.json
-kill -10 $(cat /tmp/lemonsqueezer.lock)
-
 cp ~/.cache/wal/zathuracolors ~/.config/zathura/
+
+cp ~/.cache/wal/polybar ~/.config/polybar/colors
 
 ~/.bin/set_bspwm_colors
 
