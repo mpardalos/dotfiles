c8bb423bf4de762adcf44ee8fee9581dd3c0cfd1
Michalis Pardalos <m.pardalos@gmail.com>
2019-05-04 01:17:08 +0100

    Use symlinks instead of copying for wal

--- bin/post_wal.sh
+++ bin/post_wal.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-
-cp ~/.cache/wal/*.rasi ~/.config/rofi
-
-cp ~/.cache/wal/colors.sh ~/.bin/colors.sh
-cp ~/.cache/wal/colors-rgba.sh ~/.bin/colors-rgba.sh
-
-cp ~/.cache/wal/zathuracolors ~/.config/zathura/
-
-cp ~/.cache/wal/polybar ~/.config/polybar/colors
-
 ~/.bin/set_bspwm_colors
 
