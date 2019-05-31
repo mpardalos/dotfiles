e87030fb2e0cf44c9817deaa91427fb685780f82
Michalis Pardalos <m.pardalos@gmail.com>
2019-05-31 23:02:07 +0100

    Add --noninteractive option to omf install

--- fish/install.sh
+++ fish/install.sh
@@ -5,9 +5,17 @@ MAKEDEPENDS="curl"
 
 module_install() {
     if fish -c "! type omf 2>&1 >/dev/null"; then
-        curl -L https://get.oh-my.fish | fish
+        echo
+        echo "==> Downloading omf installer"
+        curl -L https://get.oh-my.fish > /tmp/fish-install-omf
+
+        echo
+        echo "==> Running omf installer"
+        chmod u+x /tmp/fish-install-omf
+        /tmp/fish-install-omf --noninteractive
     fi
 
+    echo "==> Setting omf theme"
     fish -c "omf install agnoster"
     fish -c "omf theme agnoster"
 }
