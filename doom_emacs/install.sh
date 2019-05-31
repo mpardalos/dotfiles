0381dc88895fedb8240b73eae74f0b4481572cb0
Michalis Pardalos <m.pardalos@gmail.com>
2019-06-01 00:31:41 +0100

    Install doom-emacs in emacs installer

--- doom_emacs/install.sh
+++ doom_emacs/install.sh
@@ -1,1 +1,9 @@
 DEPENDS="emacs"
+
+module_install() {
+    printf "\n==> Cloning doom-emacs\n"
+    git clone https://github.com/hlissner/doom-emacs ~/.emacs.d
+
+    printf "\n==> Refreshing doom-emacs\n"
+    ~/.emacs.d/bin/doom refresh
+}
