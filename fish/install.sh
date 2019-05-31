3d3858a19556f9cbb5abe1a9a978a93f04302fd2
Michalis Pardalos <m.pardalos@gmail.com>
2019-05-31 23:04:47 +0100

    Hide useless output from fish module installer

--- fish/install.sh
+++ fish/install.sh
@@ -4,10 +4,10 @@ DEPENDS="fish exa"
 MAKEDEPENDS="curl"
 
 module_install() {
-    if fish -c "! type omf 2>&1 >/dev/null"; then
+    if fish -c "! type omf >/dev/null 2>&1 "; then
         echo
         echo "==> Downloading omf installer"
-        curl -L https://get.oh-my.fish > /tmp/fish-install-omf
+        curl -L https://get.oh-my.fish > /tmp/fish-install-omf 2>/dev/null
 
         echo
         echo "==> Running omf installer"
