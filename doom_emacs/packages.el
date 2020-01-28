bac4da1ab116be291001a336082f056c01867da7
Michalis Pardalos <m.pardalos@gmail.com>
2020-01-28 21:36:47 +0000

    Disable some unused emacs packages

--- doom_emacs/packages.el
+++ doom_emacs/packages.el
@@ -1,11 +1,8 @@
 ;; -*- no-byte-compile: t; -*-
 
-(package! bison-mode)
 (package! vimrc-mode)
-(package! ripgrep)
 (package! alloy-mode
     :recipe (:host github :repo "dwwmmn/alloy-mode"))
-(package! org-ref)
 (package! fill-column-indicator)
 
 (package! evil-snipe :disable t)
