e38e6318b970c9fa2c7fcceb2776c2a5af0b8f74
Michalis Pardalos <m.pardalos@gmail.com>
2020-01-29 14:14:18 +0000

    Change emacs font to Fira Code

--- doom_emacs/config.el
+++ doom_emacs/config.el
@@ -5,7 +5,7 @@
     doom-leader-key "SPC"
     doom-localleader-key "SPC SPC"
     doom-theme (my/remember-theme-read 'doom-one)
-    doom-font (font-spec :family "Source Code Pro" :size 14)
+    doom-font (font-spec :family "Fira Code" :size 14)
     doom-variable-pitch-font (font-spec :family "Noto Sans")
     +pretty-code-enabled-modes '('not 'vterm 'eshell))
     +latex-viewers '(pdf-tools zathura))
