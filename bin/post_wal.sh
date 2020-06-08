1f72d3d20f7050cbbd17e96d251d5746d70f6d37
Michalis Pardalos <m.pardalos@gmail.com>
2020-06-08 22:44:13 +0100

    Enable mako notifications in sway

--- bin/post_wal.sh
+++ bin/post_wal.sh
@@ -3,3 +3,4 @@
 pkill polybar; ~/.bin/start_polybar
 
 cat ~/.config/alacritty/alacritty-config.yml ~/.cache/wal/alacritty-colors.yml > ~/.config/alacritty.yml
+cat ~/.config/mako/base-config ~/.cache/wal/colors-mako > ~/.config/mako/config
