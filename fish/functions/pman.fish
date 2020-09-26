bc2d7dc1529220afd81bd298cf32400c8ad3cbd0
Michalis Pardalos <m.pardalos@gmail.com>
2020-09-26 20:07:22 +0100

    [fish] Make pman cmd actually work

--- fish/functions/pman.fish
+++ fish/functions/pman.fish
@@ -1,5 +1,3 @@
 function pman --description "Show a man page as a pdf document"
-    man -w $argv[1] 2>&1 >/dev/null && begin;
-        man -t $argv[1] | ps2pdf - | zathura -
-    end
+    man -Tpdf $argv[1] | zathura -
 end
