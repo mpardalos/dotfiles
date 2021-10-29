f025c0d92461dfcce0d798ca929053162840c274
Michalis Pardalos <m.pardalos@gmail.com>
2021-10-29 14:24:04 +0100

    [emacs] Check in custom.el

---
+++ doom_emacs/custom.el
@@ -0,0 +1,20 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("7eea50883f10e5c6ad6f81e153c640b3a288cd8dc1d26e4696f7d40f754cc703" "9724b3abaf500b227faa036dcf817abed9764802835ba6e8d1e475c877205157" "1704976a1797342a1b4ea7a75bdbb3be1569f4619134341bd5a4c1cfb16abad4" "5784d048e5a985627520beb8a101561b502a191b52fa401139f4dd20acb07607" "cf922a7a5c514fad79c483048257c5d8f242b21987af0db813d3f0b138dfaf53" "835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" "a7b20039f50e839626f8d6aa96df62afebb56a5bbd1192f557cb2efb5fcfb662" "d268b67e0935b9ebc427cad88ded41e875abfcc27abd409726a92e55459e0d01" "0b3aee906629ac7c3bd994914bf252cf92f7a8b0baa6d94cb4dfacbd4068391d" "333958c446e920f5c350c4b4016908c130c3b46d590af91e1e7e2a0611f1e8c5" "0466adb5554ea3055d0353d363832446cd8be7b799c39839f387abb631ea0995" "6c531d6c3dbc344045af7829a3a20a09929e6c41d7a7278963f7d3215139f6a7" "1d5e33500bc9548f800f9e248b57d1b2a9ecde79cb40c0b1398dec51ee820daf" "6c386d159853b0ee6695b45e64f598ed45bd67c47f671f69100817d7db64724d" "4f1d2476c290eaa5d9ab9d13b60f2c0f1c8fa7703596fa91b235db7f99a9441b" "c4ec44cce043a30c57dd3a1342a890991976a872cd71f4c0650c9387a66eb3f4" "2c9dc3ab1cbde9d4a3cd129b945d39e19fe7107708edec589118b14d24c01dac" "cbe59faa15a5f52bdf110b822509b1bbe7f080997c036d0ceb22675adc2e4d76" "cebda623e3f9ecd4a4af08d92c406bc4517e323d9259d5156f43901860224e30" "63bfcabeb44559c67d8827dc68cd6c4a6d3ce35ef4504343af12d42f24894e00" "f4876796ef5ee9c82b125a096a590c9891cec31320569fc6ff602ff99ed73dca" "990e24b406787568c592db2b853aa65ecc2dcd08146c0d22293259d400174e37" "fce3524887a0994f8b9b047aef9cc4cc017c5a93a5fb1f84d300391fba313743" "4a8d4375d90a7051115db94ed40e9abb2c0766e80e228ecad60e06b3b397acab" "93ed23c504b202cf96ee591138b0012c295338f38046a1f3c14522d4a64d7308" "e3c64e88fec56f86b49dcdc5a831e96782baf14b09397d4057156b17062a8848" "e074be1c799b509f52870ee596a5977b519f6d269455b84ed998666cf6fc802a" "54cf3f8314ce89c4d7e20ae52f7ff0739efb458f4326a2ca075bf34bc0b4f499" "aaa4c36ce00e572784d424554dcc9641c82d1155370770e231e10c649b59a074" "76bfa9318742342233d8b0b42e824130b3a50dcc732866ff8e47366aed69de11" "3c2f28c6ba2ad7373ea4c43f28fcf2eed14818ec9f0659b1c97d4e89c99e091e" "8f5a7a9a3c510ef9cbb88e600c0b4c53cdcdb502cfe3eb50040b7e13c6f4e78e" "e6ff132edb1bfa0645e2ba032c44ce94a3bd3c15e3929cdf6c049802cf059a2a" "08a27c4cde8fcbb2869d71fdc9fa47ab7e4d31c27d40d59bf05729c4640ce834" default))
+ '(safe-local-variable-values
+   '((lsp-haskell-formatting-provider . stylish-haskell)
+     (lsp-haskell-formatting-provider . "stylish-haskell")
+     (eval require 'org-roam-dev)
+     (eval outline-hide-sublevels 5))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(put 'narrow-to-region 'disabled nil)
+(put 'company-coq-fold 'disabled nil)
