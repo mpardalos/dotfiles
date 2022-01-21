ffda849636a886617bf4631f6141f4ed4fe96aa4
Michalis Pardalos <m.pardalos@gmail.com>
2022-01-21 19:31:58 +0200

    Add PATH config to fish

--- fish/config.fish
+++ fish/config.fish
@@ -7,4 +7,18 @@ abbr --add tree "ll --tree"
 
 set -gx VIRTUAL_ENV_DISABLE_PROMPT 1
 
+set -a PATH ~/.local/bin
+set -a PATH ~/.emacs.d/bin
+set -a PATH ~/.ghcup/bin
+set -a PATH ~/.cabal/bin
+set -a PATH ~/Documents/go/bin
+set -a PATH ~/.local/share/npm/bin
+
+set -x GOPATH "$HOME/Documents/go"
+
+set -x VISUAL /usr/bin/nvim
+set -x EDITOR /usr/bin/nvim
+
+set -x OSTYPE linux
+
 eval (direnv hook fish)
