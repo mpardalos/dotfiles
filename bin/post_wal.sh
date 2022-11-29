7bc1dace88a72e5101e6a6c7e93dc30046152839
Michalis Pardalos <m.pardalos@gmail.com>
2022-11-29 11:10:19 +0000

    Remove bin/post_wal.sh

--- bin/post_wal.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-~/.bin/set_bspwm_colors
-pkill polybar; ~/.bin/start_polybar
-
-cat ~/.config/alacritty/alacritty-config.yml ~/.cache/wal/alacritty-colors.yml > ~/.config/alacritty.yml
-
-cat ~/.config/mako/base-config ~/.cache/wal/colors-mako > ~/.config/mako/config
-makoctl reload
