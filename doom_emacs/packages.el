5b550091c0aee68e869e6ec4826336afdfad65d5
Michalis Pardalos <me@mpardalos.com>
2023-01-16 14:07:26 +0000

    [emacs] Remove (unpin! org-roam)

--- doom_emacs/packages.el
+++ doom_emacs/packages.el
@@ -16,9 +16,6 @@
 
 (package! benchmark-init)
 
-;; org-roam
-(unpin! org-roam)
-
 (package! websocket)
 (package! org-roam-ui
     :recipe (:host github :repo "org-roam/org-roam-ui" :files ("*.el" "out")))
