96b7d9115042dbdfc46cecfb38a205d2b7dbe934
Michalis Pardalos <me@mpardalos.com>
2023-01-20 14:25:47 +0000

    [emacs] Remove unused functions from autoloads.el

--- doom_emacs/autoload.el
+++ doom_emacs/autoload.el
@@ -1,31 +1,6 @@
  ;;; ~/.config/dotfiles/doom_emacs/autoload.el -*- lexical-binding: t; -*-
 
 ;;;###autoload
-(defun my/doom-refresh-reload ()
-  (interactive)
-  (with-output-to-temp-buffer "*Doom refresh*"
-    (pop-to-buffer "*Doom refresh*")
-    (make-process
-     :name "Doom refresh"
-     :buffer "*Doom refresh*"
-     :command '("/home/mpardalos/.emacs.d/bin/doom" "--yes" "refresh")
-     :sentinel (lambda (p s) (when (string= s "finished\n") (doom/reload))))))
-
-;;;###autoload
-(defun my/pop-to-dedicated-term ()
-  (interactive)
-  (if (not (get-buffer dedicated-name))
-      (save-window-excursion
-        (term "/bin/fish")
-        (rename-buffer dedicated-name)))
-  (pop-to-buffer dedicated-name))
-
-;;;###autoload
-(defun my/open-external-term ()
-  (interactive)
-  (start-process "external-term" nil "gnome-terminal"))
-
-;;;###autoload
 (defun evil-mc-vertical-align (character)
   "Aligns all cursors vertically with a given CHARACTER to the one with the
 highest colum number (the rightest).
