d1372e7a68c43084f99a4613b37bde0935735b23
Michalis Pardalos <me@mpardalos.com>
2023-09-27 11:36:21 +0100

    [emacs] safe-local-variable-values

--- doom_emacs/custom.el
+++ doom_emacs/custom.el
@@ -18,7 +18,10 @@
  '(projectile-project-root-files-bottom-up '(".ccls-root" ".projectile" ".project" ".git"))
  '(projectile-project-root-files-top-down-recurring '("compile_commands.json" "Makefile" "CVS/"))
  '(safe-local-variable-values
-   '((counsel-compile-local-builds "ssh pardalos@rs6 \"echo 'make -C /work/pardalos/vhdl.mp/npchip/tb/assembler -j' | tcsh -s\"" "ssh pardalos@rs6 \"echo 'cd /work/pardalos/vhdl.mp/np5chip/ && go -p:\"-log4 /work/pardalos/npasm_log.cfg\" -E lava' | tcsh -s\"" "ssh pardalos@rs6 \"echo 'cd /work/pardalos/vhdl.mp/np5chip/ && go -p:\"-log4 /work/pardalos/npasm_log.cfg\" -P lava' | tcsh -s\"")
+   '((lsp-haskell-server-path . "haskell-language-server")
+     (eval add-to-list 'auto-mode-alist
+      '("\\.v\\'" . verilog-mode))
+     (counsel-compile-local-builds "ssh pardalos@rs6 \"echo 'make -C /work/pardalos/vhdl.mp/npchip/tb/assembler -j' | tcsh -s\"" "ssh pardalos@rs6 \"echo 'cd /work/pardalos/vhdl.mp/np5chip/ && go -p:\"-log4 /work/pardalos/npasm_log.cfg\" -E lava' | tcsh -s\"" "ssh pardalos@rs6 \"echo 'cd /work/pardalos/vhdl.mp/np5chip/ && go -p:\"-log4 /work/pardalos/npasm_log.cfg\" -P lava' | tcsh -s\"")
      (counsel-compile-local-builds "ssh pardalos@rs6 \"echo 'make -C /work/pardalos/vhdl.mp/npchip/tb/assembler -j' | tcsh -s\"" "ssh pardalos@rs6 \"echo 'cd /work/pardalos/vhdl.mp/np5chip/ && go -E lava' | tcsh -s\"" "ssh pardalos@rs6 \"echo 'cd /work/pardalos/vhdl.mp/np5chip/ && go -P lava' | tcsh -s\"")
      (counsel-compile-local-builds "ssh pardalos@rs6 \"echo 'make -C /work/pardalos/vhdl.mp/npchip/tb/assembler -j' | tcsh -s\"")
      (counsel-compile-local-builds "./mk bringup.x86hops noarmhops" "./mk" "./regress")
