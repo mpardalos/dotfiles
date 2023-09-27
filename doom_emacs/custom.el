c99cdf1eb6c3d6ff495581fa4f3e78095dcb5493
Michalis Pardalos <me@mpardalos.com>
2023-09-27 11:37:19 +0100

    [emacs] ts-fold-replacement-face (?)

--- doom_emacs/custom.el
+++ doom_emacs/custom.el
@@ -51,7 +51,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(ts-fold-replacement-face ((t (:foreground unspecified :box nil :inherit font-lock-comment-face :weight light)))))
 (put 'narrow-to-region 'disabled nil)
 (put 'company-coq-fold 'disabled nil)
 (put 'customize-group 'disabled nil)
