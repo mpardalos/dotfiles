04dac56c7afa898b54879223052720a0bf652d0a
Michalis Pardalos <me@mpardalos.com>
2023-09-27 11:37:07 +0100

    [emacs] bibtex-entry-format?

--- doom_emacs/custom.el
+++ doom_emacs/custom.el
@@ -3,6 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(bibtex-entry-format
+   '(opts-or-alts required-fields numerical-fields whitespace sort-fields))
  '(connection-local-criteria-alist
    '(((:application eshell)
       eshell-connection-default-profile)))
