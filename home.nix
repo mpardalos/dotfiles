dfe5bd3a3a509724dec1c5f5a02538566c44e40b
Michalis Pardalos <me@mpardalos.com>
2024-06-21 19:17:52 +0100

    Add a basic home-manager flake (just packages)

---
+++ home.nix
@@ -0,0 +1,52 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "mpardalos";
+  home.homeDirectory = "/home/mpardalos";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "24.05"; # Please read the comment before changing.
+
+  home.packages = with pkgs; [
+    tectonic # LaTeX-in-a-box
+    gum # Fancy terminal UIs
+    hcloud # Hetzner cloud cli
+    fd
+    entr
+    pandoc
+    editorconfig-checker
+    aspell
+    eza
+    direnv
+    fzf
+    zoxide
+    zellij
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+  };
+
+  programs.home-manager.enable = true;
+}
