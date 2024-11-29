f62cf2fed1aa36d17139196d16fb2c3ca3299405
Michalis Pardalos <me@mpardalos.com>
2024-11-29 12:30:29 +0000

    [emacs] Use default auth-source (desktop wallet)

--- doom_emacs/custom.el
+++ doom_emacs/custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auth-sources '(default "secrets:Login"))
  '(bibtex-entry-format
    '(opts-or-alts required-fields numerical-fields whitespace sort-fields))
  '(connection-local-criteria-alist
