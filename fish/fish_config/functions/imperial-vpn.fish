a02c9949ec0091b27be38af58e80a093b15ad378
Michalis Pardalos <me@mpardalos.com>
2025-01-28 10:00:22 +0000

    [fish] Add imperial-vpn function to manage College VPN

---
+++ fish/fish_config/functions/imperial-vpn.fish
@@ -0,0 +1,14 @@
+function imperial-vpn --description 'Manage Imperial College VPN' --argument command
+    set -l profile "profile-userlocked-mp5617@ic.ac.uk.ovpn"
+
+    switch $command
+        case "up"
+            openvpn3 session-start --config $profile
+        case "down"
+            openvpn3 session-manage --disconnect --config $profile
+            openvpn3 session-manage --cleanup
+        case '*'
+            echo "Usage: imperial-vpn [up|down]"
+            return 1
+    end
+end
