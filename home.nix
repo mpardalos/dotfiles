71ba421962fb522bf9a394706d50247f53186da2
Michalis Pardalos <me@mpardalos.com>
2025-06-14 23:27:59 +0100

    [home-manager] Add symlinks

--- home.nix
+++ home.nix
@@ -52,19 +52,16 @@
     ollama
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
+  home.file = let
+    inherit (config.lib.file) mkOutOfStoreSymlink;
+    here = "${config.home.homeDirectory}/.config/dotfiles";
+  in {
+    ".config/fish".source = mkOutOfStoreSymlink "${here}/fish/fish_config";
+    ".config/nvim".source = mkOutOfStoreSymlink "${here}/neovim";
+    ".config/direnv".source = mkOutOfStoreSymlink "${here}/direnv";
+    ".config/alacritty".source = mkOutOfStoreSymlink "${here}/alacritty";
+    ".config/git".source = mkOutOfStoreSymlink "${here}/git";
+    ".config/tmux".source = mkOutOfStoreSymlink "${here}/tmux";
   };
 
   # Home Manager can also manage your environment variables through
