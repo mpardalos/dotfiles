3343e5e14a452c1376892431c9d1b2813783415b
Michalis Pardalos <me@mpardalos.com>
2025-10-22 14:53:54 +0100

    [emacs] Font stuff idk

--- emacs/custom.el
+++ emacs/custom.el
@@ -67,4 +67,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Cascadia Code" :foundry "SAJA" :slant normal :weight regular :height 120 :width normal)))))
+ '(default ((t (:family "Cascadia Code" :foundry "outline" :slant normal :weight regular :height 100 :width normal)))))
+
