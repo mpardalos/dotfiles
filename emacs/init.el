7440e2579cde1fdaf2ac7e8188d38791319bfeb2
Michalis Pardalos <me@mpardalos.com>
2025-11-07 10:05:04 +0000

    [emacs] Install corfu

--- emacs/init.el
+++ emacs/init.el
@@ -265,6 +265,29 @@ or aliases."
 (use-package envrc
   :hook (after-init . envrc-global-mode))
 
+(use-package corfu
+  ;; :custom
+  ;; (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
+  ;; (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
+  ;; (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
+  ;; (corfu-preview-current nil)    ;; Disable current candidate preview
+  ;; (corfu-preselect 'prompt)      ;; Preselect the prompt
+  ;; (corfu-on-exact-match nil)     ;; Configure handling of exact matches
+
+  ;; Enable Corfu only for certain modes. See also `global-corfu-modes'.
+  :hook prog-mode
+
+  :init
+  ;; Recommended: Enable Corfu globally.  Recommended since many modes provide
+  ;; Capfs and Dabbrev can be used globally (M-/).  See also the customization
+  ;; variable `global-corfu-modes' to exclude certain modes.
+  (global-corfu-mode)
+
+  ;; Enable optional extension modes:
+  ;; (corfu-history-mode)
+  ;; (corfu-popupinfo-mode)
+  )
+
 (use-package auctex
   :custom
   (TeX-source-correlate-start-server 'always)
