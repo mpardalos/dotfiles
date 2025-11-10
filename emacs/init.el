1073a18d6f43231108bb11e491b1b1d4ffcbc5bc
Michalis Pardalos <me@mpardalos.com>
2025-11-10 14:51:29 +0000

    [emacs] Add markdown mode

--- emacs/init.el
+++ emacs/init.el
@@ -516,6 +516,13 @@ or aliases."
   :after neocaml
   :hook tuareg-mode)
 
+(use-package markdown-mode
+  :ensure t
+  :mode ("README\\.md\\'" . gfm-mode)
+  :init (setq markdown-command "multimarkdown")
+  :bind (:map markdown-mode-map
+         ("C-c C-e" . markdown-do)))
+
 ;; Files
 (general-nmap "SPC f f" #'find-file)
 
