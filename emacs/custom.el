942ea7461a4503b9aa16f9441295aba2d44e53ed
Michalis Pardalos <me@mpardalos.com>
2025-11-10 14:52:51 +0000

    [emacs] (Rocq-mode) Change last-goal-request face

--- emacs/custom.el
+++ emacs/custom.el
@@ -73,4 +73,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Cascadia Code" :foundry "SAJA" :slant normal :weight regular :height 120 :width normal)))))
+ '(default ((t (:family "Cascadia Code" :foundry "SAJA" :slant normal :weight regular :height 120 :width normal))))
+ '(rocq-mode-last-goal-request ((t (:underline (:color foreground-color :style dots :position nil))))))
