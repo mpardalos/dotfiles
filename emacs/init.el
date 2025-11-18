81799ef4a2bf92624d89c2c483395b9e1888432b
Michalis Pardalos <me@mpardalos.com>
2025-11-18 15:24:23 +0000

    [emacs] Add orderless

--- emacs/init.el
+++ emacs/init.el
@@ -235,6 +235,16 @@ or aliases."
   :init
   (vertico-mode))
 
+(use-package orderless
+  :custom
+  ;; Configure a custom style dispatcher (see the Consult wiki)
+  ;; (orderless-style-dispatchers '(+orderless-consult-dispatch orderless-affix-dispatch))
+  ;; (orderless-component-separator #'orderless-escapable-split-on-space)
+  (completion-styles '(orderless basic))
+  (completion-category-overrides '((file (styles partial-completion))))
+  (completion-category-defaults nil) ;; Disable defaults, use our settings
+  (completion-pcm-leading-wildcard t))
+
 (use-package marginalia
   :init (marginalia-mode))
 
