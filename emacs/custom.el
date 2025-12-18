7839390fc25212d4207b82418b37642d6ce62312
Michalis Pardalos <me@mpardalos.com>
2025-12-18 11:09:40 +0000

    [emacs] Remove Rocq custom-set-faces from custom.el

--- emacs/custom.el
+++ emacs/custom.el
@@ -72,5 +72,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(rocq-mode-last-goal-request ((((background dark)) :background "DarkGreen") (((background light)) :background "DarkSeaGreen1")))
- '(rocq-mode-processing-face ((((background dark)) :background "gray30") (((background light)) :background "light gray"))))
+ )
