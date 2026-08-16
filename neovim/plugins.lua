263c6306d3c596f031d6af41fdc5d52bd0e95e47
Michalis Pardalos <me@mpardalos.com>
2026-08-16 20:58:45 +0100

    [nvim] Use built-in plugin manager

---
+++ neovim/plugins.lua
@@ -0,0 +1,27 @@
+vim.pack.add({
+  -- General {{{
+  "https://github.com/pseewald/vim-anyfold",
+  "https://github.com/ervandew/supertab",
+  "https://github.com/easymotion/vim-easymotion",
+  "https://github.com/tpope/vim-surround",
+  "https://github.com/junegunn/vim-easy-align",
+  "https://github.com/scrooloose/nerdtree",
+  "https://github.com/tpope/vim-commentary",
+  "https://github.com/tpope/vim-repeat",
+  -- }}}
+
+  -- Tmux {{{
+  "https://github.com/benmills/vimux",
+  "https://github.com/christoomey/vim-tmux-navigator",
+  -- }}}
+
+  -- Visuals {{{
+  "https://github.com/chriskempson/base16-vim",
+  "https://github.com/ryanoasis/vim-devicons",
+  "https://github.com/dylanaraps/wal.vim",
+  -- }}}
+
+  -- Language-specific {{{
+  "https://github.com/dag/vim-fish",
+  -- }}}
+})
