9c0b5a3536282cbc2e965bf3fd80d53346bc9b0a
Michalis Pardalos <me@mpardalos.com>
2026-08-25 19:44:31 +0100

    [rgb] Fix detection of RAM sticks on odin

--- nix/modules/rgb.nix
+++ nix/modules/rgb.nix
@@ -1,5 +1,7 @@
 {
   nixos = {pkgs, ...}: {
+    # Required on Gigabyte motherboards (which I have)
+    boot.kernelParams = [ "acpi_enforce_resources=lax" ];
     services.hardware.openrgb = {
       enable = true;
       package = pkgs.openrgb-with-all-plugins;
