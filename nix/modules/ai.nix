3337e6cbacb2c043c9cb7a1fb65312d28a544178
Michalis Pardalos <me@mpardalos.com>
2026-08-27 08:35:06 +0100

    [ai] Add default permission config to opencode

--- nix/modules/ai.nix
+++ nix/modules/ai.nix
@@ -3,9 +3,20 @@
     programs.claude-code.enable = true;
     programs.antigravity-cli.enable = true;
     programs.pi-coding-agent.enable = true;
+
+    # Opencode
     programs.opencode = {
       enable = true;
       tui.theme = "system";
+      settings.permission = {
+        "*" = "ask";
+        webfetch = "allow";
+        websearch = "allow";
+        bash = {
+          "grep *" = "allow";
+          "git log *" = "allow";
+        };
+      };
     };
   };
 }
