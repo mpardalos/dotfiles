a7240f31ba71bdb6b07db2af6eee2c9131ecf75d
Michalis Pardalos <me@mpardalos.com>
2026-09-05 13:05:48 +0100

    [hm] Override opencode version (1.18.29)

--- nix/modules/ai.nix
+++ nix/modules/ai.nix
@@ -1,12 +1,30 @@
 {
-  home-manager = {
+  home-manager = { pkgs, ... }: {
     programs.claude-code.enable = true;
     programs.antigravity-cli.enable = true;
     programs.pi-coding-agent.enable = true;
 
-    # Opencode
     programs.opencode = {
       enable = true;
+      ### Remove when 1.18.29 hits nixos-unstable ###
+      package = pkgs.opencode.overrideAttrs (
+        finalAttrs: prevAttrs: {
+          version = "1.18.29";
+          src = pkgs.fetchFromGitHub {
+            owner = "anomalyco";
+            repo = "opencode";
+            tag = "v${finalAttrs.version}";
+            hash = "sha256-lCXlxTOhcX70jxJAbpolyGlIxQK2nst+6bFhq3Xzdmc=";
+          };
+          passthru = prevAttrs.passthru // {
+            node_modules = prevAttrs.passthru.node_modules.overrideAttrs (nodeAttrs: {
+              buildPhase = builtins.replaceStrings [ ''--cpu="*"'' ''--os="*"'' ] [ "" "" ] nodeAttrs.buildPhase;
+              outputHash = "sha256-xr2EUM1Fo9c0/Xgc//bEzSwC7mMawmTlzcEZqrfuhag=";
+            });
+          };
+        }
+      );
+      ###############################################
       tui.theme = "system";
       settings.plugin = [ "@mohak34/opencode-notifier@0.2.8" ];
       settings.permission = {
